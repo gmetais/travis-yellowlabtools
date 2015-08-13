bcbbb0b2b977fbb5bad621859191ae6f0cfbe468
Gaël Métais <gael.metais@gmail.com>
2015-08-13 15:56:46 +0200

    Add some more examples

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,19 @@ module.exports = function(grunt) {
                     'https://twitter.com/tos'
                 ],
                 failConditions: [
-                    'fail if at least one url has a global score < 90/100'
+                    // These are examples, adapt to your needs
+                    'fail if at least one url has a global score < 80/100',
+                    'fail if at least one url has an imageOptimization score < 30/100',
+                    'fail if at least one url has a fileMinification score < 30/100',
+                    'fail if at least one url has a jsErrors > 0',
+                    'fail if at least one url has a notFound > 0',
+                    'fail if at least one url has a multipleRequests > 0',
+                    'fail if at least one url has a DOMaccessesOnScroll > 0',
+                    'fail if at least one url has a documentWriteCalls > 0',
+                    'fail if at least one url has a consoleMessages > 0',
+                    'fail if at least one url has a cssRules > 3000',
+                    'fail if at least one url has a cssImports > 0',
+                    'fail if at least one url has a cssDuplicatedSelectors > 50'
                 ],
                 options: {
                     device: 'desktop' // 'desktop', 'tablet' or 'phone'
@@ -34,7 +46,19 @@ module.exports = function(grunt) {
                     'https://twitter.com/tos'
                 ],
                 failConditions: [
-                    'fail if at least one url has a global score < 90/100'
+                    // These are examples, adapt to your needs
+                    'fail if at least one url has a global score < 80/100',
+                    'fail if at least one url has an imageOptimization score < 30/100',
+                    'fail if at least one url has a fileMinification score < 30/100',
+                    'fail if at least one url has a jsErrors > 0',
+                    'fail if at least one url has a notFound > 0',
+                    'fail if at least one url has a multipleRequests > 0',
+                    'fail if at least one url has a DOMaccessesOnScroll > 0',
+                    'fail if at least one url has a documentWriteCalls > 0',
+                    'fail if at least one url has a consoleMessages > 0',
+                    'fail if at least one url has a cssRules > 3000',
+                    'fail if at least one url has a cssImports > 0',
+                    'fail if at least one url has a cssDuplicatedSelectors > 50'
                 ],
                 options: {
                     device: 'phone' // 'desktop', 'tablet' or 'phone'
